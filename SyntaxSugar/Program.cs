6388214882b7cf3d767e8ae1d5e418a19cacd948
zee6197 <zeeman@berkeley.edu>
2022-04-26 11:15:41 -0700

    First commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var TheAnswer = 4;
+            var TheResponse = (TheAnswer < 9) ? $"{TheAnswer} is less than nine" : $"{TheAnswer} is greater than nine";
+            Console.WriteLine(TheResponse);
+        }
+    }
+}
